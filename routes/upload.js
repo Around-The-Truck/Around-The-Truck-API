3f4d641e3517e93cbe69d8292b0203d5ffc90b02
Kimsangho <rlaace423@gmail.com>
2014-11-30 21:04:07 +0900

    test2

--- routes/upload.js
+++ routes/upload.js
@@ -3,4 +3,17 @@ var im = require('imagemagick');
 
 exports.upload = function(req, res){
 	console.log("Received file:\n" + JSON.stringify(req.files));
+
+	console.log(req.files.image.originalFilename);
+ 	console.log(req.files.image.path);
+ 	return;
+    fs.readFile(req.files.image.path, function (err, data){
+    	var dirname = "/home/rajamalw/Node/file-upload";
+    	var newPath = dirname + "/uploads/" +   req.files.image.originalFilename;
+    	fs.writeFile(newPath, data, function (err) {
+    	if(err){
+    		res.json({'response':"Error"});
+    }else {
+    	res.json({'response':"Saved"});
+	}
 };
